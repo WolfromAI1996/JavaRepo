fc940e2873959dca4053bae561fe158594f54599
Yasitha <Yasitha@wolfrom.org>
2024-02-21 05:33:43 +0000

    New User Add

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
-public class Main {
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    break;
+  }
+  System.out.println(i);
+}ublic class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+
+
 }
